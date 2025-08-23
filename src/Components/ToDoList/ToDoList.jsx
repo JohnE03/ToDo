d85e1ef2b6c07be8f9b11a54ac1924b53c7279a7
JohnE03 <2101519@eng.asu.edu.eg>
2025-08-23 16:29:07 +0300

    ToDOList Component Added anc css module for styling

---
+++ src/Components/ToDoList/ToDoList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TodoCard from "../TodoCard/TodoCard";
+import styles from "./TodoList.module.css";
+
+function TodoList({ todos, onDelete }) {
+  if (todos.length === 0) {
+    return <p className={styles.empty}>No todos yet 🙃</p>;
+  }
+
+  return (
+    <div className={styles.list}>
+      {todos.map((t) => (
+        <TodoCard key={t.id} todo={t} onDelete={onDelete} />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
